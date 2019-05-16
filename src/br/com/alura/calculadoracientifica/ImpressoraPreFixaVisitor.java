b33bb3c8527ec464c3d387a7bb3870f7c611a7f9
Douglas Schwingel <dgls.sch@gmail.com>
2019-05-16 14:53:59 -0300

    Criando o Visitor para contas no formato  preFixo

---
+++ src/br/com/alura/calculadoracientifica/ImpressoraPreFixaVisitor.java
@@ -0,0 +1,56 @@
+package br.com.alura.calculadoracientifica;
+
+public class ImpressoraPreFixaVisitor implements Visitor {
+
+    @Override
+    public void visitaSoma(Soma soma) {
+        System.out.print("+ ");
+        System.out.print("(");
+        soma.getEsquerda().aceita(this);
+        System.out.print(" ");
+        soma.getDireita().aceita(this);
+        System.out.print(")");
+    }
+
+    @Override
+    public void visitaSubstracao(Subtracao subtracao) {
+        System.out.print("- ");
+        System.out.print("(");
+        subtracao.getEsquerda().aceita(this);
+        System.out.print(" ");
+        subtracao.getDireita().aceita(this);
+        System.out.print(")");
+    }
+
+    @Override
+    public void visitaDivisao(Divisao divisao) {
+        System.out.print("/ ");
+        System.out.print("(");
+        divisao.getEsquerda().aceita(this);
+        System.out.print(" ");
+        divisao.getDireita().aceita(this);
+        System.out.print(")");
+    }
+
+
+    @Override
+    public void visitaNumero(Numero numero) {
+        System.out.print(numero.getNumero());
+    }
+
+    @Override
+    public void visitaMultiplicacao(Multiplicacao multiplicacao) {
+        System.out.print("* ");
+        System.out.print("(");
+        multiplicacao.getEsquerda().aceita(this);
+        System.out.print(" ");
+        multiplicacao.getDireita().aceita(this);
+        System.out.print(")");
+    }
+
+    @Override
+    public void visitaRaizQuadrada(RaizQuadrada raizQuadrada) {
+        System.out.print("\u221A");
+        raizQuadrada.getExpressao().aceita(this);
+    }
+}
--- src/br/com/alura/calculadoracientifica/Programa.java
+++ src/br/com/alura/calculadoracientifica/Programa.java
@@ -1,22 +1,27 @@
 package br.com.alura.calculadoracientifica;
 
+import br.com.alura.calculadora.Impressora;
+
 public class Programa {
 
-    private static Visitor impressora;
-
     public static void main(String[] args) {
-        impressora = new ImpressoraVisitor();
+        ImpressoraVisitor impressora = new ImpressoraVisitor();
+        ImpressoraPreFixaVisitor preFixa = new ImpressoraPreFixaVisitor();
         Expressao esquerda = new Subtracao(new Soma(new Numero(10), new Numero(15)), new Numero(3));
         Expressao direita = new Soma(new Numero(2), new Numero(10));
         Expressao soma = new Soma(esquerda, direita);
 
-        calculaEImprime(soma);
-        calculaEImprime(new Divisao(new Numero(8), new Soma(new Numero(2), new Numero(2))));
-        calculaEImprime(new Multiplicacao(new Numero(5), new Numero(25)));
-        calculaEImprime(new RaizQuadrada(new Soma(new Numero(2), new Numero(2))));
+        calculaEImprime(impressora, soma);
+        calculaEImprime(preFixa, soma);
+        calculaEImprime(impressora, new Divisao(new Numero(8), new Soma(new Numero(2), new Numero(2))));
+        calculaEImprime(preFixa, new Divisao(new Numero(8), new Soma(new Numero(2), new Numero(2))));
+        calculaEImprime(impressora, new Multiplicacao(new Numero(5), new Numero(25)));
+        calculaEImprime(preFixa, new Multiplicacao(new Numero(5), new Numero(25)));
+        calculaEImprime(impressora, new RaizQuadrada(new Soma(new Numero(2), new Numero(2))));
+        calculaEImprime(preFixa, new RaizQuadrada(new Soma(new Numero(2), new Numero(2))));
     }
 
-    private static void calculaEImprime(Expressao tipoOperacao) {
+    private static void calculaEImprime(Visitor impressora, Expressao tipoOperacao) {
         System.out.println("\n*************************");
         tipoOperacao.aceita(impressora);
         System.out.print(" = ");
